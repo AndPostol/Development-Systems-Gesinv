f3edc077b5e4aec4db42c97824d51886bb186e44
josefa3 <jnhs3993@gmail.com>
2023-02-17 16:20:13 -0400

    Creación de Producto Service y Contracts

---
+++ DevSys.Gesinv.Logic/Services/ProductoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevSys.Gesinv.Models;
+using DevSys.Gesinv.Logic.Contracts;
+using DevSys.Gesinv.DAL.Contracts;
+
+namespace DevSys.Gesinv.Logic.Services
+{
+    public class ProductoService : GenericService<Producto>, IProductoService
+    {
+        public ProductoService(IGenericRepository<Producto> respository) : base(respository)
+        {
+        }
+    }
+}
+
--- DevSys.Gesinv.UI/Program.cs
+++ DevSys.Gesinv.UI/Program.cs
@@ -1,11 +1,18 @@
 using DevSys.Gesinv.DAL.DataContext;
 using Microsoft.EntityFrameworkCore;
 using DevSys.Gesinv.Models;
+using DevSys.Gesinv.Logic.Contracts;
+using DevSys.Gesinv.Logic.Services;
+using DevSys.Gesinv.DAL.Contracts;
+using DevSys.Gesinv.DAL.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IGenericRepository<Producto>, GenericRepository<Producto>>();
+builder.Services.AddScoped<IProductoService, ProductoService>();
 
 builder.Services.AddDbContext<DbInventarioContext>(options =>
 {
