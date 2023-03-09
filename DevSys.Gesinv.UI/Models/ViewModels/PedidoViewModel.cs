7b366a7af3bd3c28562192b0e64ac7ce773dbf78
yzalaya <77127813+yzalaya@users.noreply.github.com>
2023-03-09 11:12:57 -0400

    Limpiando codigo ViewCreate

--- DevSys.Gesinv.UI/Models/ViewModels/PedidoViewModel.cs
+++ DevSys.Gesinv.UI/Models/ViewModels/PedidoViewModel.cs
@@ -4,7 +4,7 @@
   {
     public int Id { get; set; }
     public bool Estatus { get; set; }
-    public int LineaPedidoId { get; set; }
+    //public int LineaPedidoId { get; set; }
     public int ProductoId { get; set; }
     public int Cantidad { get; set; }
   }
