aeef41b1e73aa59cf0e5c7f773a2f4c601317f4e
andres apostol <aandresaapostol@hotmail.com>
2023-03-10 09:11:02 -0400

    Cambios al estilo en el apartado de producto

--- DevSys.Gesinv.UI/Program.cs
+++ DevSys.Gesinv.UI/Program.cs
@@ -114,6 +114,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Producto}/{action=Index}/{id?}");
 
 app.Run();
