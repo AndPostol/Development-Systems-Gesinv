256b26bca9d00d5531da37a87dfd88b2baaea2c5
JavierGalviz <jegalviz2111@gmail.com>
2023-03-10 20:39:59 -0400

    Merge IEmpresaService con Develop

---
+++ DevSys.Gesinv.Logic/Contracts/IEmpresaService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevSys.Gesinv.Models;
+
+namespace DevSys.Gesinv.Logic.Contracts
+{
+    public interface IEmpresaService
+    {
+        public interface IEmpresaService : IGenericService<Empresa> 
+        { 
+        }
+    }
+}
